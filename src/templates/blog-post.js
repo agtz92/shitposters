28823f51a64ee0a3ad1c90d5f3bfcca81ce703cb
agtz92 <alfredo.gtz92@gmail.com>
2021-03-23 20:59:26 -0600

    metatags

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,12 +1,50 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
+import { Helmet } from "react-helmet"
+import logoChico from "../images/logo_small.png"
 import Layout from "../layouts/layout"
 import Heading from "../components/heading"
 import kebabCase from "lodash/kebabCase"
 
 export default function BlogPost({ data }) {
   const post = data.markdownRemark
+  const perro = "https://www.10datos.com/" + frontmatter.slug + "/";
   return (
+    <React.Fragment>
+    <Helmet >
+                <meta charSet="utf-8" />
+                <title>"{frontmatter.title} | 10datos"</title>
+                <meta name="description" content={frontmatter.short_description} />
+                <script type="application/ld+json">
+                  {`
+                {
+                  "@context": "https://schema.org",
+                  "@type": "BlogPosting",
+                  "mainEntityOfPage": {
+                    "@type": "WebPage",
+                    "@id":\"${perro}\"
+                  },
+                  "headline": \"${frontmatter.title}\",
+                  "description": \"${frontmatter.short_description}\",
+                  "image": "https://www.10datos.com/assets/${frontmatter.featuredimage}",  
+                  "author": {
+                    "@type": "Organization",
+                    "name": "antesdelexamen.com"
+                  },  
+                  "publisher": {
+                    "@type": "Organization",
+                    "name": "antesdelexamen",
+                    "logo": {
+                      "@type": "ImageObject",
+                      "url": \"https://www.10datos.com/${logoChico}\"
+                    }
+                  },
+                  "datePublished": \"${frontmatter.date}\",
+                  "dateModified": \"${frontmatter.date}\"
+                }
+                `}
+                </script>
+    </Helmet>
     <Layout>
       <div className="blog-post-container">
         <div className="div-grey-post"></div>
@@ -30,6 +68,7 @@ export default function BlogPost({ data }) {
       </div>
       
     </Layout>
+    </React.Fragment>
   )
 }
 
